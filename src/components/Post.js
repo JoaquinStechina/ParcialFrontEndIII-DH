928ebe591ec2d7bfef842fca4abd88df2593ccef
JoaquinStechina <joaquinstechina12@gmail.com>
2022-11-08 16:25:51 -0300

    Agrego parcial de Front

---
+++ src/components/Post.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+// El componente Post no tiene componentes hijos.
+// ESTADO: Post debe tener un número para almacenar la cantidad de likes, la misma se la defina el padre a la hora de crearlo.
+// MÉTODOS: Post debe tener un método para manejar el click de su boton, el cual debe:
+//  - sumar 1 like al posteo
+//  - sumar en la cantidad total de likes contabilizada por App
+//  - mostrar en consola qué posteo sumó un like
+// PROPS: Post recibe todos los campos que muestra en pantalla: titulo, texto, likes y el método heredados para sumar en el total.
+// Maqueta de Post:
+//    h3
+//    p
+//    button       (este boton debe permitir sumar likes)
+//    h5    (este h5 debe mostrar la cantidad de likes, pero si la misma es mayor a 10 debe decir "Más de 10 likes")
+
+export default function Post({titulo, texto, likes, metodo}) {
+  const [cantidadLikes,setCantidadLike] = useState(likes);
+
+  const agregarLikes=()=>{
+    setCantidadLike(cantidadLikes+1);
+    metodo();
+    console.log("Suma 1 like " + titulo);
+  }
+
+  return (
+    <div className='posteo'>
+      <h3>{titulo}</h3>
+      <p>{texto}</p>
+      <button onClick={agregarLikes}>👍</button>
+      <h5>{cantidadLikes > 10 ? "Más de 10 likes" : "Likes: "+ cantidadLikes}</h5>
+    </div>
+  )
+}
